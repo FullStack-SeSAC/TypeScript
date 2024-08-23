7983b444dd538576780685ccdd9c302394e9dc7b
aixion1506 <aixion1506@gmail.com>
2024-08-23 14:47:03 +0900

    게시글 TS로 리팩토링

--- 35-typescript-react/src/App.tsx
+++ 35-typescript-react/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <div className="App">
       <TodoList />
+      <hr />
+
+      <PostList />
     </div>
   );
 }
